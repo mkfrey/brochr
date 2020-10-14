36267292fa85d3a7bbd44a55b2e2a28bc42f6e35
Markus Frey <mfrey@mailbox.org>
2020-10-14 17:17:33 +0200

    Update REWE API user agent to current app version

--- app/src/main/java/io/l8/brochr/api/rewe/Constants.kt
+++ app/src/main/java/io/l8/brochr/api/rewe/Constants.kt
@@ -2,7 +2,7 @@ package io.l8.brochr.api.rewe
 
 interface Constants {
     companion object {
-        const val USER_AGENT = "REWE-Mobile-App/3.4.17 Android/8.1.0"
+        const val USER_AGENT = "REWE-Mobile-App/3.4.32 Android/9 (Smartphone)"
         const val MARKET_IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Logo_REWE.svg/320px-Logo_REWE.svg.png"
         const val OFFER_QUERY_URL = "https://mobile-api.rewe.de/products/offer-search?marketId="
         const val MARKET_QUERY_URL = "https://mobile-api.rewe.de/mobile/markets/market-search?query="
