a7bc063229b2a8431c092bbbb4ed8dece34b180c
Markus Frey <mfrey@mailbox.org>
2022-01-28 22:07:25 +0100

    Update Gradle and apply recommended code improvements

--- app/src/main/java/io/l8/brochr/api/rewe/deserialize/OfferQuery.kt
+++ app/src/main/java/io/l8/brochr/api/rewe/deserialize/OfferQuery.kt
@@ -8,25 +8,27 @@ import io.l8.brochr.models.Shop
 import kotlinx.serialization.json.Json
 import okhttp3.ResponseBody
 
-class OfferQueryDeserializer(val resultReceiver: Shop.OfferQueryResultReceiver) :
+class OfferQueryDeserializer(private val resultReceiver: Shop.OfferQueryResultReceiver) :
     Request.OnRequestResponse {
+    private val json = Json { ignoreUnknownKeys = true }
+
     override fun onResponse(responseBody: ResponseBody?) {
         responseBody?.let { body ->
             body.contentType()?.let { contentType ->
                 // Check the ContentType, CloudFlare might answer instead of the API
                 println(contentType.toString())
                 if (contentType.type == Constants.RESPONSE_CONTENT_TYPE && contentType.subtype == Constants.OFFER_QUERY_RESPONSE_CONTENT_SUBTYPE) {
-                    val deserializationResult = Json{ ignoreUnknownKeys = true }.decodeFromString(
+                    val deserializationResult = json.decodeFromString(
                         io.l8.brochr.api.rewe.api_model.OfferQueryResult.serializer(),
                         body.string()
                     )
                     body.close()
 
-                    val categories = deserializationResult?._meta?.categories
+                    val categories = deserializationResult._meta.categories
                     val categoryIdMap = mutableMapOf<String, Int>()
                     val categoryNames = mutableListOf<String>()
 
-                    categories?.forEachIndexed { index, offerCategory ->
+                    categories.forEachIndexed { index, offerCategory ->
                         categoryNames.add(index, offerCategory.name)
                         categoryIdMap[offerCategory.id] = index
                     }
